2645a48910dfd2374b9e090bef8d465df3478ad7
Rhana <159554135+RHAN4@users.noreply.github.com>
2025-10-13 11:07:58 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
-// Importação das suas PÁGINAS (verifique os nomes e caminhos)
+// Importação das suas PÁGINAS (verifique se os nomes e caminhos estão corretos)
 import TelaInicial from './pages/TelaInicial'; 
 import TelaLogin from './pages/TelaLogin';
-import TelaCadastro from './pages/TelaCadastro'; 
 import TelaSobre from './pages/TelaEquipe';
 import TelaContato from './pages/TelaEquipe';
 import TelaDashboard from './pages/TelaDashboard'; 
+import TelaCadastro from './pages/TelaCadastro'; 
+
+// --- NOVAS ROTAS DE CADASTRO ---
+import TelaCadastroEstagiario from './pages/TelaCadastroEstagiario';
+import TelaCadastroFaculdade from './pages/TelaCadastroFaculdade';
+import TelaCadastroFuncionario from './pages/TelaCadastroFuncionario';
+
 
 function App() {
   return (
     <AuthProvider>
-      {/* O <Router> foi removido daqui */}
       <Routes>
         {/* --- ROTAS PÚBLICAS --- */}
         <Route path="/" element={<TelaInicial />} />
         <Route path="/sobre" element={<TelaSobre />} />
         <Route path="/contato" element={<TelaContato />} />
         <Route path="/login" element={<TelaLogin />} />
+        
+        {/* --- ROTAS DE CADASTRO --- */}
         <Route path="/cadastro" element={<TelaCadastro />} />
+        <Route path="/cadastroEstagiario" element={<TelaCadastroEstagiario />} />
+        <Route path="/cadastroFaculdade" element={<TelaCadastroFaculdade />} />
+        <Route path="/cadastroFuncionario" element={<TelaCadastroFuncionario />} />
         
         {/* --- ROTA PROTEGIDA (PRIVADA) --- */}
         <Route 
